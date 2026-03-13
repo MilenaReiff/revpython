55f3c5970c3ee39b0bba1fc6fa1a80ac310a28c4
Milena Reiff <milenareiff@gmail.com>
2026-03-13 14:34:08 -0300

    Add files via upload

---
+++ revisão.py
@@ -0,0 +1,43 @@
+#1)
+#notas = ['7.5', '8', '6', '9.5', '5']
+#notas.append ('8.5')
+#notas[4] = '6.5'
+#notas.sort(reverse=True)
+#maior = max(notas)
+#menor = min(notas)
+#print(notas)
+#print("Esse é o maior número da lista:", maior)
+#print("Esse é o menor número da lista:"menor)
+#2)
+#nomes = ["Isabela", "Tiago", "Milena", "Aniger", "Heitor"]
+#for i, nomes in enumerate(nomes):
+    #print("Aluno", f'{i}: {nomes}')
+#3)
+#numeros = [3, 17, 8, 42, 5, 100, 23, 6, 11, 99]
+#for num in numeros:
+    #if num % 2==0:
+        #print (num)
+#for n in numeros:
+    #if n>20:
+        #print(n)
+#soma = sum(numeros)
+#print(soma)
+#4)
+#numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+#print(numeros[:4])
+#print(numeros[7:])
+#print(numeros[::2])
+#5)
+turma = [["Tiaguinho tigrinho", 2.8], ["Nigeria", 9.4], ["Marreta", 7.2], ["Bernardete", 5.6]]
+for nome, nota in turma:
+    print(f"{nome} tirou {nota}")
+
+    
+
+
+
+    
+
+
+
+
